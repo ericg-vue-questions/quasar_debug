a951c1f7cf0aefbc22beacb0a5a2a499d07e752d
Eric Gorr <ericgorr@gmail.com>
2019-03-12 09:16:07 -0400

    add a function to debug

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,6 +1,10 @@
 <template>
   <q-page class="flex flex-center">
     <img alt="Quasar logo" src="~assets/quasar-logo-full.svg">
+
+    <div v-for="value in returnArray()" :key="value.id">
+      {{value}}<br />
+    </div>
   </q-page>
 </template>
 
@@ -9,6 +13,14 @@
 
 <script>
 export default {
-  name: 'PageIndex'
+  name: 'PageIndex',
+
+  methods: {
+    returnArray () {
+      debugger
+      console.log('in returnArray')
+      return Array(5).fill().map((x, i) => i)
+    }
+  }
 }
 </script>
